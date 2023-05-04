bf7c2925be786e954f693d5a5c55e65a6d149c03
Dmytro Oseledko <d.oseledko@outlook.com>
2023-05-04 21:54:19 +0300

    users

---
+++ project_quotes/users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views import View
+
+from .forms import RegisterForm
+
+
+# Create your views here.
+
+class RegisterView(View):
+    form_class = RegisterForm
+    template_name = 'users/signup.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {"form": self.form_class})
+
+    def post(self, request):
+        ...
