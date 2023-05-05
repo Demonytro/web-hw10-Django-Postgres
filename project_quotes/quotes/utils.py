4069dabfcf206acb2e08413c53d1341a7f8031b2
Dmytro Oseledko <d.oseledko@outlook.com>
2023-05-05 15:46:59 +0300

    users

--- project_quotes/quotes/utils.py
+++ project_quotes/quotes/utils.py
@@ -19,3 +19,8 @@ def get_po333stgresdb():
     db = client.hw10
 
     return db
+def get_po444stgresdb():
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.hw10
+
+    return db
--- project_quotes/quotes/views.py
+++ project_quotes/quotes/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 
 # Create your views here.
-from .utils import get_mongodb
+# from .utils import get_mongodb
+from .models import Quote
 
 
 def main(request, page=1):
-    db = get_mongodb()
-    quotes = db.quotes.find()
+    # db = get_mongodb()
+    # quotes = db.quotes.find()
+    quotes = Quote.objects.all()
     per_page = 10
     paginator = Paginator(list(quotes), per_page)
     quotes_on_page = paginator.page(page)
