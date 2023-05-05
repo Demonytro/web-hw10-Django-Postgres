438c4d9facd5f520978ae98f5ef00f5d85ba1b30
Dmytro Oseledko <d.oseledko@outlook.com>
2023-05-05 14:38:44 +0300

    migrate to postgres

---
+++ project_quotes/quotes/utils.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+
+
+def get_mongodb():
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.hw10
+
+    return db
+
+
+def get_postgresdb():
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.hw10
+
+    return db
+
+def get_po333stgresdb():
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.hw10
+
+    return db
